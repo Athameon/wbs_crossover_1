97ddd80fe87048f077c5beca0cc98af4d38bbb1d
Athameon <Benjamin.Nopper@freenet.de>
2021-06-10 15:22:12 +0200

    Added some basic components.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import './App.css';
 import React, { useState, useEffect, useRef } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Header from './components/Header'
+import Error from './components/Error'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import LoadingComponent from './components/LoadingComponent'
 
 function App() {
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false)
   return (
     <div>
-    {/* <Header />
-    <Switch>
-        <Route path="/error" component={Error} />
-        <Route path="/">
-        { isLoading? 
-            <LoadingComponent /> :
-            isError?
-            <Redirect to="/error" /> :
-            content && <Main />}
-        </Route>
-    </Switch>
-    <Footer /> */}
-</div>
+      <Header />
+      <Switch>
+          <Route path="/error" component={Error} />
+          <Route path="/">
+          { isLoading? 
+              <LoadingComponent /> :
+              isError?
+                <Redirect to="/error" /> :
+                <Main />}
+          </Route>
+      </Switch>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/LoadingComponent.js
@@ -0,0 +1,10 @@
+import React from'react';
+import './LoadingComponent.css'
+
+const LoadingComponent = () => {
+  return(
+    <h1>LoadingComponent</h1>
+  )
+}
+
+export default LoadingComponent;
