ff6221f56030281bb7430745615eb889779211ef
Athameon <Benjamin.Nopper@freenet.de>
2021-06-10 14:54:21 +0200

    Installed packages and added route switch.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          text
-        </p>
-      </header>
-    </div>
+    <div>
+    {/* <Header />
+    <Switch>
+        <Route path="/error" component={Error} />
+        <Route path="/">
+        { isLoading? 
+            <LoadingComponent /> :
+            isError?
+            <Redirect to="/error" /> :
+            content && <Main />}
+        </Route>
+    </Switch>
+    <Footer /> */}
+</div>
   );
 }
 
