34ec149e29649d1c7df8ff2d59001a2c00e22646
Minja Popovic <mini.me@mein.gmx>
2021-06-11 13:37:24 +0200

    main page styling implemented

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -1,10 +1,19 @@
 import React from'react';
 import './LoadingComponent.css'
+import HashLoader from 'react-spinners/HashLoader'
+import { css } from "@emotion/react";
+
+const hash = css`
+position: absolute;
+top: 45%;
+left: 45%;
+transform: translate (50%, -50%);
+`
 
 const LoadingComponent = () => {
   return(
     <div className='loading'>
-      <img src='https://image.spreadshirtmedia.net/image-server/v1/compositions/T31A1PA29PT10X0Y16D136468399FS2152Cx000000%3Ax787878/views/1,width=650,height=650,appearanceId=1,backgroundColor=ffffff/mit-diesem-sign-kannst-du-dein-persoenliches-loading-shirt-kreieren-um-deine-ganz-eigenen-textideen-umzusetzen-gibt-es-auch-ohne-text-loading-und-j.jpg' />
+      <HashLoader css={hash} size={100} color={'#C8553D'} speedMultiplier={1} />
     </div>
 
   )
