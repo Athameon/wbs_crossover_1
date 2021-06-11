756d497e5302e9b7ed5652345f37fc3e23f34260
Athameon <Benjamin.Nopper@freenet.de>
2021-06-11 08:18:13 +0200

    Moved fetch from Feed to Main. Implemented fetch with fake API call instead of a mock. Implemented url manipulation for search.
    Co-authored-by: Minja <minime89-maker@users.noreply.github.com>

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,59 +1,24 @@
 import "./Feed.css";
-import FeedMock from "../Feed.json";
-import { useEffect, useState } from "react";
 import Message from "./Message";
 import UserMessage from "./UserMessage";
 
-const Feed = ({id}) => {
-  const [feedData, setFeedData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false); //TODO: true
-  const [isError, setIsError] = useState(false);
-  useEffect(() => {
-    setFeedData(FeedMock);
-  }, []);
+const Feed = ({data, userMessages}) => {
 
-  console.log('id', id);
-
-  // useEffect(() => {
-  //   setIsError(false);
-  //   setIsLoading(true);
-
-  //   fetch('id')
-  //   .then(result => {
-  //     if (result.ok) {
-  //       return result.json();
-  //     }
-  //     throw Error('Error occured during fetching data.');
-  //   }, (networkError) => {
-  //     throw Error(networkError);
-  //   })
-  //   .then(jsonResult => {
-  //     setUser(jsonResult);
-
-  //     setIsLoading(false);
-  //   })
-  //   .catch(error => {
-  //     console.error(error);
-
-  //     setIsLoading(false);
-  //     setIsError(true);
-  //   })
-  // }, [])
-  if (feedData) { // TODO: Remove after real fetch is implemented
+  if (data) {
     return (
       <>
-        {feedData.messages.map((item) => {
+        {data.messages.map((item) => {
           return (
-            <>
-              {id? <UserMessage {...item} /> :
+            <div key={item.id}>
+              {userMessages? <UserMessage {...item} /> :
                 <Message {...item} />}
-            </>
+            </div>
           );
         })}
       </>
     );
   }
-  return <p></p>;
+  return <p>Data in Feed is falsy.</p>;
 };
 
 export default Feed;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,70 @@
-import React, { useState} from'react';
-import './Main.css'
-import User from './User.js'
-import FeedMock from "../Feed.json";
-import Feed from './Feed';
+import React, { useState, useEffect } from "react";
+import "./Main.css";
+import LoadingComponent from './LoadingComponent';
+import Error from './Error.js'
+import Feed from "./Feed";
 
-const Main = () => {
-  return (
+const Main = (props) => {
+  const [searchResult, setSearchResult] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false)
+
+  useEffect(() => {
+    let url = "/Feed.json";  // Replace by real API address
+    console.log('Triggered fetch data wiht search value: ' ,props.searchValue)
+    if (props.searchValue) {
+      switch (props.searchValue.charAt(0)) {
+        case "#":
+          url += "?hastag=" + props.searchValue.substr(1, props.searchValue.length - 1);
+          break;
+        case "@":
+          url += "?user=" + props.searchValue.substr(1, props.searchValue.length - 1);
+          break;
+        default:
+          url +=
+            "?mustContainString=" +
+            props.searchValue.substr(1, props.searchValue.length - 1);
+      }
+    }
+
+    setIsError(false);
+    setIsLoading(true);
+
+    fetch(url)
+    .then(result => {
+      if (result.ok) {
+        return result.json();
+      }
+      throw Error('Error occured during fetching data.');
+    }, (networkError) => {
+      throw Error(networkError);
+    })
+    .then(jsonResult => {
+      console.log('Fetched following data from the API: ', jsonResult);
+      setSearchResult(jsonResult);
+
+      setIsLoading(false);
+    })
+    .catch(error => {
+      console.error(error);
+
+      setIsLoading(false);
+      setIsError(true);
+    })
+
+  }, [props.searchValue]);
+
+  if (isLoading) {
+    return <LoadingComponent />;
+  } else if(isError) {
+    return <Error />
+  } else {
+    return (
 		<>
-      <Feed />
+      <Feed data={searchResult} />
 	  </>
-  )
-}
+    )
+  }
+};
 
 export default Main;
